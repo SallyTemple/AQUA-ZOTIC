54c190e9d45131233895c5ec31ea3c964e3d7ba3
SallyTemple <pfende@gmail.com>
2018-07-04 01:26:00 -0700

    update

--- src/components/Bootstrap.js
+++ src/components/Bootstrap.js
@@ -57,6 +57,16 @@ const Bootstrap = () => (
             </div>
           </li>
           <li className="nav-item">
+            <a className="nav-link" href="#">
+              Contact Us <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Relaxation <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
             <a className="nav-link " href="#">
               Administration
             </a>
