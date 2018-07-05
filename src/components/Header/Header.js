43eff164e1ca1e7b5ec9a29114c0ddcf36665f57
SallyTemple <pfende@gmail.com>
2018-07-04 21:01:48 -0700

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = props => (
   <header className="header">
     <h1>Aqua-Zotic</h1>
     <h2>
-      The Most Premier Exotic Freshwater Aquarium Fish Store In Phoenix Metro Area!!!
+      The Most Premier Exotic Freshwater Aquarium Fish Store In Arizona!
     </h2>
   </header>
 );
