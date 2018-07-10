c9c564014c3e97d1a2cfc67da50851b13c940ae6
SallyTemple <pfende@gmail.com>
2018-07-09 20:47:24 -0700

    update

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Player } from 'video-react';
-
+import Title from "../Title";
+import Header from "../Header";
 
 
 const Home = (props) => (
       <div>
-        
+        <Header >
+          <Title />
+        </Header>
+     
+          <img src="https://i.ytimg.com/vi/--ztGaF4m2U/maxresdefault.jpg " alt="W3Schools.com"/>
+
       <div className="jumbotron">
-      <h1>Aqua-Zotic</h1>
+  
   
    
       </div> 
